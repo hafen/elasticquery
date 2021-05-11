961fb621d0e681ab6f4c67a8bb9afe4404a511ac
Ryan Hafen <rhafen@gmail.com>
2021-05-11 16:35:24 -0700

    Add simple_query_string filter and allow boolean filter options

--- R/filter.R
+++ R/filter.R
@@ -8,12 +8,16 @@
 #' @param field field name (see [queryable_fields()] for all possibilities)
 #' @param from the lower value of the range
 #' @param to the upper value of the range
+#' @param bool one of "must", "filter", "must_not", "should" (see [here](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-bool-query.html))
 #' @export
 filter_range <- function(
-  query, field, from = NULL, to = NULL
+  query, field, from = NULL, to = NULL,
+    bool = c("must", "filter", "must_not", "should")
 ) {
   check_class(query, c("query_agg", "query_fetch"), "filter_range")
-  check_fields(query, field, "filter_range")
+  if (query$check_fields)
+    check_fields(query, field, "filter_range")
+  bool <- match.arg(bool)
   # TODO: make sure variable is numeric or date
   # long, integer, short, byte, double, float, half_float, scaled_float
 
@@ -26,7 +30,7 @@ filter_range <- function(
   if (!is.null(to))
     res$range[[field]]$lte <- to
 
-  query$filters <- c(query$filters, list(res))
+  query$filters[[bool]] <- c(query$filters[[bool]], list(res))
   query
 }
 # { "range": { "processedOnDate": { "gte": "2020-02-01" }}}
@@ -36,16 +40,22 @@ filter_range <- function(
 #' @param query a [query_agg()] or [query_fetch()] object
 #' @param field field name (see [queryable_fields()] for all possibilities)
 #' @param match a string to match
+#' @param bool one of "must", "filter", "must_not", "should" (see [here](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-bool-query.html))
 #' @export
-filter_match <- function(query, field, match) {
+filter_match <- function(
+  query, field, match,
+  bool = c("must", "filter", "must_not", "should")
+) {
   check_class(query, c("query_agg", "query_fetch"), "filter_match")
-  check_fields(query, field, "filter_match")
+  if (query$check_fields)
+    check_fields(query, field, "filter_match")
+  bool <- match.arg(bool)
   res <- list(
     match = list()
   )
   res$match[[field]] <- match
 
-  query$filters <- c(query$filters, list(res))
+  query$filters[[bool]] <- c(query$filters[[bool]], list(res))
   query
 }
 # { "match": { "fullText": "and" }},
@@ -55,17 +65,23 @@ filter_match <- function(query, field, match) {
 #' @param query a [query_agg()] or [query_fetch()] object
 #' @param field field name (see [queryable_fields()] for all possibilities)
 #' @param regexp a regular expression string to match
+#' @param bool one of "must", "filter", "must_not", "should" (see [here](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-bool-query.html))
 #' @export
-filter_regexp <- function(query, field, regexp) {
+filter_regexp <- function(
+  query, field, regexp,
+  bool = c("must", "filter", "must_not", "should")
+) {
   check_class(query, c("query_agg", "query_fetch"), "filter_match")
-  check_fields(query, field, "filter_regexp")
+  if (query$check_fields)
+    check_fields(query, field, "filter_regexp")
+  bool <- match.arg(bool)
 
   res <- list(
     regexp = list()
   )
   res$regexp[[field]] <- regexp
 
-  query$filters <- c(query$filters, list(res))
+  query$filters[[bool]] <- c(query$filters[[bool]], list(res))
   query
 }
 
@@ -75,21 +91,56 @@ filter_regexp <- function(query, field, regexp) {
 #' @param field field name (see [queryable_fields()] for all possibilities)
 #' @param terms a string or vector of strings to exact match
 #' @importFrom jsonlite toJSON
+#' @param bool one of "must", "filter", "must_not", "should" (see [here](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-bool-query.html))
 #' @export
-filter_terms <- function(query, field, terms) {
+filter_terms <- function(
+  query, field, terms,
+  bool = c("must", "filter", "must_not", "should")
+) {
   check_class(query, c("query_agg", "query_fetch"), "filter_terms")
-  check_fields(query, field, "filter_terms")
+  if (query$check_fields)
+    check_fields(query, field, "filter_terms")
+  bool <- match.arg(bool)
 
   tm <- ifelse(length(terms) == 1, "term", "terms")
   res <- list()
   res[[tm]] <- list()
   res[[tm]][[field]] <- terms
 
-  query$filters <- c(query$filters, list(res))
+  query$filters[[bool]] <- c(query$filters[[bool]], list(res))
   query
 }
 
-# { "term":  { "languageCode": "en" }},
-# { "terms":  { "languageCode": ["en", "au"] }},
 
-# logic = c("and", "or", "not")
+#' Specify a "simple query string" filter
+#'
+#' @param query a [query_agg()] or [query_fetch()] object
+#' @param fields field names (see [queryable_fields()] for all possibilities)
+#' @param string a string or vector of strings to exact match
+#' @param bool one of "must", "filter", "must_not", "should" (see [here](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-bool-query.html))
+#' @details See [here](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-simple-query-string-query.html) for more.
+#' More of the options will be implemented in the future.
+#' Adding more options and testing them could be an easy exercise for a
+#' contributor's pull request.
+#' @export
+filter_sqs <- function(
+  query, fields, string,
+  bool = c("must", "filter", "must_not", "should")
+) {
+  check_class(query, c("query_agg", "query_fetch"), "filter_sqs")
+  if (query$check_fields)
+    check_fields(query, fields, "filter_sqs")
+  bool <- match.arg(bool)
+
+  # if (!is.null(query$sqs))
+  #   message("Simple query string has already been specified. Overwriting...")
+
+  res <- list(simple_query_string = list(
+    fields = as.list(fields),
+    query = string
+  ))
+
+  query$filters[[bool]] <- c(query$filters[[bool]], res)
+
+  query
+}
